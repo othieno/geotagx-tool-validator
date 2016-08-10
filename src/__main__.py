7769e63fb6148ff925688f84c3f73323b081d928
Jeremy Othieno <j.othieno@gmail.com>
2016-08-10 16:33:54 +0200

    Implement the 'get_argparser' function

--- src/__main__.py
+++ src/__main__.py
@@ -40,13 +40,47 @@ def run(arguments):
     raise NotImplementedError()
 
 
-def get_argparser():
-    """Constructs the application's command-line argument parser.
+def get_argparser(subparsers=None):
+    """Constructs the application's command-line argument parser. The validator tool
+    is a standalone program but also a part of the GeoTag-X toolkit which means
+    that its arguments can be sub-commands to a specific command. For more information,
+    check out: https://docs.python.org/2/library/argparse.html#sub-commands
+
+    Args:
+        subparsers (argparse._SubParsersAction): If specified, the argument parser is
+            created as a parser for a program command, and not the actual program.
 
     Returns:
         argparse.ArgumentParser: A command-line argument parser instance.
+
+    Raises:
+        TypeError: If subparsers is not None or an argparse._SubParsersAction instance.
     """
-    raise NotImplementedError()
+    import argparse
+
+    parser = None
+    parser_arguments = {
+        "description": "Validate your GeoTag-X projects.",
+        "add_help": False,
+    }
+    if subparsers is None:
+        parser = argparse.ArgumentParser(prog="geotagx-validator", **parser_arguments)
+    elif isinstance(subparsers, argparse._SubParsersAction):
+        parser = subparsers.add_parser("validate", help="Validate your GeoTag-X projects.", **parser_arguments)
+        parser.set_defaults(run=run)
+    else:
+        raise TypeError("Invalid parameter type: get_argparser expects 'argparse._SubParsersAction' but got '{}'.".format(type(subparsers).__name__))
+
+    options = parser.add_argument_group("OPTIONS")
+    options.add_argument("-h", "--help", action="help", help="Display this help and exit.")
+    options.add_argument("-i", "--ignore-missing", action="store_true", help="Ignore missing configuration files.")
+    options.add_argument("-q", "--quiet", action="store_true", help="Suppress all warnings.")
+    options.add_argument("-v", "--verbose", action="store_true", help="Detail the actions being performed.")
+    options.add_argument("-V", "--version", action="version", help="Display version information and exit.", version=_version())
+
+    parser.add_argument("paths", metavar="PATH", nargs="+")
+
+    return parser
 
 
 def _version():
