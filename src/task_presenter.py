296b506521172ed6aa3b73aba5023450a0ffdb40
Jeremy Othieno <j.othieno@gmail.com>
2017-03-01 22:32:06 +0100

    Implement the is_task_presenter_configuration function

--- src/task_presenter.py
+++ src/task_presenter.py
@@ -44,4 +44,76 @@ def is_task_presenter_configuration(configuration, enable_logging=False):
     elif not isinstance(enable_logging, bool):
         raise TypeError("Invalid argument type: is_task_presenter_configuration expects 'bool' for the enable_logging argument but got '{}'.".format(type(enable_logging).__name__))
 
+    missing = [k for k in ["questionnaire"] if k not in configuration]
+    if missing:
+        return (False, "The task presenter configuration is missing the following fields: '{}'.".format("', '".join(missing)))
+
+    validators = {
+        "language": is_task_presenter_language,
+        "subject": is_task_presenter_subject,
+        "questionnaire": is_task_presenter_questionnaire,
+    }
+    for key, configuration in configuration.iteritems():
+        validator = validators.get(key)
+        if not validator:
+            return (False, "The task presenter configuration key '{}' is not recognized.".format(key))
+
+        valid, message = validator(configuration)
+        if not valid:
+            return (False, message)
+
+    return (True, None)
+
+
+def is_task_presenter_language(language):
+    """Validates the specified language configuration.
+
+    A valid language configuration is comprised of the following fields:
+    - available: a list of available languages where each language is identified by a code,
+    - default: a task presenter's default language, also identified by a unique code. The
+      default language must also be a member of the list of available languages.
+
+    Args:
+        language (dict): A task presenter language configuration to validate.
+
+    Returns:
+        <bool, str|None>: A pair containing the value True if the specified configuration
+            valid, False otherwise; as well as an error message in case it is invalid.
+    """
+    raise NotImplementedError()
+
+
+def is_task_presenter_subject(subject):
+    """Validates the specified subject configuration.
+
+    A valid subject configuration is comprised of the following fields:
+    - type: a string denoting the subject type.
+
+    Args:
+        subject (dict): A task presenter subject configuration to validate.
+
+    Returns:
+        <bool, str|None>: A pair containing the value True if the specified configuration
+            is valid, False otherwise; as well as an error message in case it is invalid.
+    """
+    raise NotImplementedError()
+
+
+def is_task_presenter_questionnaire(questionnaire, available_languages=None):
+    """Validates the specified questionnaire configuration.
+
+    A valid questionnanire configuration is comprised of the following fields:
+    - questions: a list of question configurations.
+
+    Args:
+        questionnanire (dict): A task presenter questionnanire configuration to validate.
+        available_languages (list): A list of available languages.
+
+    Returns:
+        <bool, str|None>: A pair containing the value True if the specified configuration
+            is valid, False otherwise; as well as an error message in case it is invalid.
+
+    Raises:
+        TypeError: If the 'available_languages' argument is not a list.
+    """
     raise NotImplementedError()
